e8601b49ed3f3274ee8036b96696d7eb9690eccb
melvin.mihaylov <melvin.mihaylov@flatrocktech.com>
2023-09-04 10:33:20 +0300

    Images fixed 2.0

--- src/components/ProductSlider.vue
+++ src/components/ProductSlider.vue
@@ -2,12 +2,11 @@
     <div class="carousel relative container mx-auto" style="max-width: 1600px">
         <div class="carousel-inner relative overflow-hidden w-full">
             <!--Slide 1-->
-            <input class="carousel-open" type="radio" id="carousel-1" name="carousel" aria-hidden="true" hidden=""
+            <input class="carousel-open" type="radio" id="carousel-1" name="carousel" aria-hidden="true" hidden
                 checked="checked" />
             <div class="carousel-item absolute opacity-0" style="height: 50vh">
-                <div class="block h-full w-full mx-auto pt-6 md:pt-0 md:items-center bg-cover bg-right"
-                    style="background-image: url('../../public/images/slider_images/pregrajdeniq-na-terasa.jpg');">
-                </div>
+                <img class="block h-full w-full mx-auto pt-6 md:pt-0 md:items-center bg-cover bg-center"
+                    src="../../public/images/slider_images/pregrajdeniq-na-terasa.jpg" alt="">
             </div>
             <label for="carousel-3"
                 class="prev control-1 w-10 h-10 ml-2 md:ml-10 absolute cursor-pointer hidden text-3xl font-bold text-black hover:text-white rounded-full bg-white hover:bg-gray-900 leading-tight text-center z-10 inset-y-0 left-0 my-auto">‹</label>
@@ -15,11 +14,10 @@
                 class="next control-1 w-10 h-10 mr-2 md:mr-10 absolute cursor-pointer hidden text-3xl font-bold text-black hover:text-white rounded-full bg-white hover:bg-gray-900 leading-tight text-center z-10 inset-y-0 right-0 my-auto">›</label>
 
             <!--Slide 2-->
-            <input class="carousel-open" type="radio" id="carousel-2" name="carousel" aria-hidden="true" hidden="" />
+            <input class="carousel-open" type="radio" id="carousel-2" name="carousel" aria-hidden="true" hidden />
             <div class="carousel-item absolute opacity-0 bg-cover bg-center" style="height: 50vh">
-                <div class="block h-full w-full mx-auto pt-6 md:pt-0 md:items-center bg-cover bg-center"
-                    style="background-image: url('../../public/images/slider_images/ograda.jpg');">
-                </div>
+                <img class="block h-full w-full mx-auto pt-6 md:pt-0 md:items-center bg-cover bg-center"
+                    src="../../public/images/slider_images/ograda.jpg" alt="">
             </div>
             <label for="carousel-1"
                 class="prev control-2 w-10 h-10 ml-2 md:ml-10 absolute cursor-pointer hidden text-3xl font-bold text-black hover:text-white rounded-full bg-white hover:bg-gray-900 leading-tight text-center z-10 inset-y-0 left-0 my-auto">‹</label>
@@ -27,11 +25,10 @@
                 class="next control-2 w-10 h-10 mr-2 md:mr-10 absolute cursor-pointer hidden text-3xl font-bold text-black hover:text-white rounded-full bg-white hover:bg-gray-900 leading-tight text-center z-10 inset-y-0 right-0 my-auto">›</label>
 
             <!--Slide 3-->
-            <input class="carousel-open" type="radio" id="carousel-3" name="carousel" aria-hidden="true" />
+            <input class="carousel-open" type="radio" id="carousel-3" name="carousel" aria-hidden="true" hidden />
             <div class="carousel-item absolute opacity-0" style="height: 50vh">
-                <div class="block h-full w-full mx-auto pt-6 md:pt-0 md:items-center bg-cover bg-top"
-                    style="background-image: url('../../public/images/slider_images/klasicheska-ograda.jpg');">
-                </div>
+                <img class="block h-full w-full mx-auto pt-6 md:pt-0 md:items-center bg-cover bg-center"
+                    src="../../public/images/slider_images/klasicheska-ograda.jpg" alt="">
             </div>
             <label for="carousel-2"
                 class="prev control-3 w-10 h-10 ml-2 md:ml-10 absolute cursor-pointer hidden text-3xl font-bold text-black hover:text-white rounded-full bg-white hover:bg-gray-900 leading-tight text-center z-10 inset-y-0 left-0 my-auto">‹</label>
@@ -42,15 +39,15 @@
             <ol class="carousel-indicators">
                 <li class="inline-block mr-3">
                     <label for="carousel-1"
-                        class="carousel-bullet cursor-pointer block text-4xl text-gray-400 hover:text-gray-900">•</label>
+                        class="carousel-bullet cursor-pointer block text-4xl text-white hover:text-gray-900">•</label>
                 </li>
                 <li class="inline-block mr-3">
                     <label for="carousel-2"
-                        class="carousel-bullet cursor-pointer block text-4xl text-gray-400 hover:text-gray-900">•</label>
+                        class="carousel-bullet cursor-pointer block text-4xl text-white hover:text-gray-900">•</label>
                 </li>
                 <li class="inline-block mr-3">
                     <label for="carousel-3"
-                        class="carousel-bullet cursor-pointer block text-4xl text-gray-400 hover:text-gray-900">•</label>
+                        class="carousel-bullet cursor-pointer block text-4xl text-white hover:text-gray-900">•</label>
                 </li>
             </ol>
         </div>
