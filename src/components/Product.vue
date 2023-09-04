df94735f8aa31f7d5894d3e5819c49cc10e0e874
melvin.mihaylov <melvin.mihaylov@flatrocktech.com>
2023-09-04 10:18:31 +0300

    Images fix

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -1,8 +1,8 @@
 
 <template>
-    <div v-for="product in products" :key="product.name" class="w-full md:w-1/3 xl:w-1/4 p-8 flex flex-col">
+    <div v-for="product in products" :key="product.name" class="w-full h-96 md:w-1/3 xl:w-1/4 p-8 flex flex-col">
         <a href="#">
-            <img class="hover:grow hover:shadow-lg" :src="product.imgSRC" />
+            <img class="hover:grow hover:shadow-lg" style="width: 500px; height: 250px;" :src="product.imgSRC" />
             <div class="flex items-center justify-between">
                 <div class="flex items-center gap-2 m-1">
                     <i class="fa-solid fa-location-dot fa-beat-fade"></i>
@@ -26,25 +26,25 @@ export default {
                 {
                     location: "Варна",
                     name: "Врата от ковано желязо",
-                    imgSRC: "https://scontent.fsof8-1.fna.fbcdn.net/v/t39.30808-6/363404818_795632062352171_3276481335776882310_n.jpg?stp=dst-jpg_p180x540&_nc_cat=108&ccb=1-7&_nc_sid=5614bc&_nc_ohc=97mKAQrhL7oAX8ZSzwy&_nc_ht=scontent.fsof8-1.fna&oh=00_AfDaPsSLD68oepDQWGJyUqgtE3rrC9Qwlr8KNiN34VhNEg&oe=64F5660E",
+                    imgSRC: "./images/zelena-vrata.jpg",
                     category: "Врати"
                 },
                 {
                     location: "Варна",
                     name: "Навес за две парко места в двора на КООП",
-                    imgSRC: "https://scontent.fsof8-1.fna.fbcdn.net/v/t39.30808-6/359824485_784636536785057_5931790654023804415_n.jpg?stp=dst-jpg_p180x540&_nc_cat=103&ccb=1-7&_nc_sid=5614bc&_nc_ohc=eRZOTRmnAawAX-oWsoV&_nc_ht=scontent.fsof8-1.fna&oh=00_AfC30uq8JMaQeh3KknpTYwVsyeYNY6HKLDcYmBPnUm6vNQ&oe=64F47E99",
+                    imgSRC: "./images/naves-za-dve-parko-mesta.jpg",
                     category: "Навеси"
                 },
                 {
                     location: "с. Любен Каравелово",
                     name: "Изработка и монтаж на козирка, с покритие от поликарбонатна плоскост",
-                    imgSRC: "https://scontent.fsof8-1.fna.fbcdn.net/v/t39.30808-6/352277768_766917315223646_5426047907203570548_n.jpg?stp=dst-jpg_p180x540&_nc_cat=105&ccb=1-7&_nc_sid=5614bc&_nc_ohc=HJHFU2RnxfoAX-b4pPP&_nc_ht=scontent.fsof8-1.fna&oh=00_AfA8KshLQOPBSkEfLCbDK1lx0vA1cpz_bkUtwEshKpbbaw&oe=64F62826",
+                    imgSRC: "./images/izrabotka-i-montaj-na-kozirka.jpg",
                     category: "Козирки"
                 },
                 {
                     location: "с. Изворско",
                     name: "Класически метален парапет в с. Изворско.",
-                    imgSRC: "https://scontent.fsof8-1.fna.fbcdn.net/v/t39.30808-6/352757337_766087961973248_8513692638980697219_n.jpg?stp=dst-jpg_s600x600&_nc_cat=104&ccb=1-7&_nc_sid=5614bc&_nc_ohc=a3BfV6gIDO4AX9u5BZX&_nc_ht=scontent.fsof8-1.fna&oh=00_AfDhhTf0SMKyr9sxY8G3u5EVgH6Blls7sy5q31-YNh3Yeg&oe=64F57584",
+                    imgSRC: "./images/klasicheski-metalen-parapet.jpg",
                     category: "Парапет"
                 },
             ]
--- src/components/ProductSlider.vue
+++ src/components/ProductSlider.vue
@@ -5,9 +5,8 @@
             <input class="carousel-open" type="radio" id="carousel-1" name="carousel" aria-hidden="true" hidden=""
                 checked="checked" />
             <div class="carousel-item absolute opacity-0" style="height: 50vh">
-                <div class="block h-full w-full mx-auto flex pt-6 md:pt-0 md:items-center bg-cover bg-right" style="
-              background-image: url('https://scontent.fsof8-1.fna.fbcdn.net/v/t39.30808-6/330396333_1653995548399342_2509358545454718031_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=5614bc&_nc_ohc=BDj2yjQBoJYAX8p8AhO&_nc_oc=AQk8SjY_Pu2s5SAqH6LueQb7bcJ21HK2gHdC4mTtvgTxYbXoNEbU5jQAeM5kj-_AcsY&_nc_ht=scontent.fsof8-1.fna&oh=00_AfDVQghqR0lmPHBYg7zSDWEgie_FQWSnCzgMiF0ylk9ZDw&oe=64F65447');
-            ">
+                <div class="block h-full w-full mx-auto pt-6 md:pt-0 md:items-center bg-cover bg-right"
+                    style="background-image: url('../../public/images/slider_images/pregrajdeniq-na-terasa.jpg');">
                 </div>
             </div>
             <label for="carousel-3"
@@ -18,9 +17,8 @@
             <!--Slide 2-->
             <input class="carousel-open" type="radio" id="carousel-2" name="carousel" aria-hidden="true" hidden="" />
             <div class="carousel-item absolute opacity-0 bg-cover bg-center" style="height: 50vh">
-                <div class="block h-full w-full mx-auto flex pt-6 md:pt-0 md:items-center bg-cover bg-top" style="
-              background-image: url('https://scontent.fsof8-1.fna.fbcdn.net/v/t39.30808-6/352757337_766087961973248_8513692638980697219_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=5614bc&_nc_ohc=a3BfV6gIDO4AX9u5BZX&_nc_ht=scontent.fsof8-1.fna&oh=00_AfBNKWhGNyQu_WfTtNgiaSYYCnp1nbXFtO9lRkE-7hNPnA&oe=64F57584');
-            ">
+                <div class="block h-full w-full mx-auto pt-6 md:pt-0 md:items-center bg-cover bg-center"
+                    style="background-image: url('../../public/images/slider_images/ograda.jpg');">
                 </div>
             </div>
             <label for="carousel-1"
@@ -29,11 +27,10 @@
                 class="next control-2 w-10 h-10 mr-2 md:mr-10 absolute cursor-pointer hidden text-3xl font-bold text-black hover:text-white rounded-full bg-white hover:bg-gray-900 leading-tight text-center z-10 inset-y-0 right-0 my-auto">›</label>
 
             <!--Slide 3-->
-            <input class="carousel-open" type="radio" id="carousel-3" name="carousel" aria-hidden="true" hidden="" />
+            <input class="carousel-open" type="radio" id="carousel-3" name="carousel" aria-hidden="true" />
             <div class="carousel-item absolute opacity-0" style="height: 50vh">
-                <div class="block h-full w-full mx-auto flex pt-6 md:pt-0 md:items-center bg-cover bg-center" style="
-              background-image: url('https://scontent.fsof8-1.fna.fbcdn.net/v/t39.30808-6/298564858_559346049314108_2565536477650828037_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=5614bc&_nc_ohc=ttntzlxvss4AX_aPQCJ&_nc_oc=AQkECEpUSckqhkPxJ1sF5KEN4SaejqSqkjmFgtMQFSxrZBJyCrd2smtuEpnOH_oAib4&_nc_ht=scontent.fsof8-1.fna&oh=00_AfCQHa5bWAvBuER1bnTtFq6m5cYRB0fxxbR5uL_YRyyN8g&oe=64F4F2F0');
-            ">
+                <div class="block h-full w-full mx-auto pt-6 md:pt-0 md:items-center bg-cover bg-top"
+                    style="background-image: url('../../public/images/slider_images/klasicheska-ograda.jpg');">
                 </div>
             </div>
             <label for="carousel-2"
