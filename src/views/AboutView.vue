2b70c441d67e795a3bad3fc4c15db85709c61149
MelvinMihaylov <melvinmihaylov@gmail.com>
2023-09-08 00:33:38 +0300

    Dark theme removed

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,10 +1,10 @@
 <template>
   <section class="pt-24 bg-gray-900">
-    <div class="grid py-8 px-4 mx-auto max-w-screen-xl lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+    <div class="grid py-8 px-4 mx-auto max-w-screen-xl lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12 text-white">
       <div class="place-self-center mr-auto lg:col-span-7">
-        <h1 class="mb-10 max-w-2xl text-2xl font-extrabold leading-none md:text-4xl xl:text-4xl dark:text-white">
+        <h1 class="mb-10 max-w-2xl text-2xl font-extrabold leading-none md:text-4xl xl:text-4xl">
           Каталог за метални изделия и конструкции</h1>
-        <p class="mb-6 max-w-2xl font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
+        <p class="mb-6 max-w-2xl font-light lg:mb-8 md:text-lg lg:text-xl">
           Добре дошли в света на изкуството и елегантността на изделията от метал!
           <br><br>
           Нашият уебсайт е мястото, където може да откриете уникални и изискани изделия, които ще преобразят вашия дом и
@@ -14,7 +14,7 @@
           нашите изделия от метал. Не се колебайте да се свържете с нас за всякакви въпроси и консултации.
         </p>
         <a href="/"
-          class="inline-flex justify-center items-center py-3 px-5 mr-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
+          class="inline-flex justify-center items-center py-3 px-5 mr-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 hover:text-yellow-400">
           Към каталога
           <svg class="ml-2 -mr-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
             <path fill-rule="evenodd"
@@ -23,7 +23,7 @@
           </svg>
         </a>
         <a href="#footer"
-          class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-gray-900 rounded-lg border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
+          class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center rounded-lg border border-gray-300 hover:bg-gray-500 hover:text-yellow-400 focus:ring-4 focus:ring-gray-100">
           Обадете се
         </a>
       </div>
@@ -33,41 +33,34 @@
     </div>
   </section>
 
-  <section class="bg-white dark:bg-gray-900">
+  <section class="bg-gray-900">
     <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16">
-      <h2
-        class="mb-8 text-xl md:text-4xl tracking-tight leading-tight text-center text-gray-500 lg:mb-16 dark:text-gray-500">
+      <h2 class="mb-8 text-xl md:text-4xl tracking-tight leading-tight text-center text-gray-400 lg:mb-16">
         Качествени изделия от метал
       </h2>
-      <div
-        class="flex justify-evenly md:text-xl text-gray-500 sm:gap-12 md:grid-cols-3 lg:grid-cols-6 dark:text-gray-400">
+      <div class="flex justify-evenly md:text-xl sm:gap-12 md:grid-cols-3 lg:grid-cols-6 text-gray-400">
         <a href="#" class="flex justify-center items-center">
-          <div class="md:text-4xl text-xs h-9 hover:text-gray-900 dark:hover:text-white" viewBox="0 0 125 35"
-            fill="currentColor">
+          <div class="md:text-4xl text-xs h-9 hover:text-white" viewBox="0 0 125 35" fill="currentColor">
             <h1>Врати</h1>
           </div>
         </a>
         <a href="#" class="flex justify-center items-center">
-          <div class="md:text-4xl text-xs h-9 hover:text-gray-900 dark:hover:text-white" viewBox="0 0 125 35"
-            fill="currentColor">
+          <div class="md:text-4xl text-xs h-9 hover:text-white" viewBox="0 0 125 35" fill="currentColor">
             <h1>Огради</h1>
           </div>
         </a>
         <a href="#" class="flex justify-center items-center">
-          <div class="md:text-4xl text-xs h-9 hover:text-gray-900 dark:hover:text-white" viewBox="0 0 125 35"
-            fill="currentColor">
+          <div class="md:text-4xl text-xs h-9 hover:text-white" viewBox="0 0 125 35" fill="currentColor">
             <h1>Парапети</h1>
           </div>
         </a>
         <a href="#" class="flex justify-center items-center">
-          <div class="md:text-4xl text-xs h-9 hover:text-gray-900 dark:hover:text-white" viewBox="0 0 125 35"
-            fill="currentColor">
+          <div class="md:text-4xl text-xs h-9 hover:text-white" viewBox="0 0 125 35" fill="currentColor">
             <h1>Надвеси</h1>
           </div>
         </a>
         <a href="#" class="flex justify-center items-center">
-          <div class="md:text-4xl text-xs h-9 hover:text-gray-900 dark:hover:text-white" viewBox="0 0 125 35"
-            fill="currentColor">
+          <div class="md:text-4xl text-xs h-9 hover:text-white" viewBox="0 0 125 35" fill="currentColor">
             <h1>Конструкции</h1>
           </div>
         </a>
@@ -75,69 +68,69 @@
     </div>
   </section>
 
-  <section class="bg-gray-50 dark:bg-gray-800">
+  <section class="bg-gray-800">
     <div class="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
       <div class="mb-8 max-w-screen-md lg:mb-16">
-        <h2 class="mb-4 text-4xl font-extrabold text-gray-900 dark:text-white">Нашият опит в индустрията
+        <h2 class="mb-4 text-4xl font-extrabold text-white">Нашият опит в индустрията
         </h2>
-        <p class="text-gray-500 sm:text-xl dark:text-gray-400">През годините ние сме развили уменията си и сме
+        <p class="sm:text-xl text-gray-400">През годините ние сме развили уменията си и сме
           изработили множество проекти, които са отличени с уникален дизайн и качество. Ние сме ангажирани с
           вдъхновяване на нашите клиенти и създаване на изключителни изживявания в тяхното пространство.</p>
       </div>
       <div class="space-y-8 md:grid md:grid-cols-2 lg:grid-cols-3 md:gap-12 md:space-y-0">
         <div>
           <div
-            class="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
+            class="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 bg-primary-900">
             <i class="fa-solid fa-thumbs-up text-2xl md:text-5xl"></i>
           </div>
-          <h3 class="mb-2 text-xl font-bold dark:text-white">Качество</h3>
-          <p class="text-gray-500 dark:text-gray-400">Ние се гордеем с нашето безкомпромисно качество, което винаги е на
+          <h3 class="mb-2 text-xl font-bold text-white">Качество</h3>
+          <p class="text-gray-400">Ние се гордеем с нашето безкомпромисно качество, което винаги е на
             първо място във всичко, което правим.</p>
         </div>
         <div>
           <div
-            class="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
+            class="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 bg-primary-900">
             <i class="fa-regular fa-handshake text-2xl md:text-5xl"></i>
           </div>
-          <h3 class="mb-2 text-xl font-bold dark:text-white">Професионализъм</h3>
-          <p class="text-gray-500 dark:text-gray-400">Нашите професионалисти са добре подготвени и ангажирани с
+          <h3 class="mb-2 text-xl font-bold text-white">Професионализъм</h3>
+          <p class="text-gray-400">Нашите професионалисти са добре подготвени и ангажирани с
             предоставянето на най-добрите решения за нашите клиенти.</p>
         </div>
         <div>
           <div
-            class="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
+            class="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 bg-primary-900">
             <i class="fa-solid fa-hand-holding-dollar text-2xl md:text-5xl"></i>
           </div>
-          <h3 class="mb-2 text-xl font-bold dark:text-white">Цена</h3>
-          <p class="text-gray-500 dark:text-gray-400">Ние сме уверени, че нашите цени са конкурентни и разумни. Ние се
+          <h3 class="mb-2 text-xl font-bold text-white">Цена</h3>
+          <p class="text-gray-400">Ние сме уверени, че нашите цени са конкурентни и разумни. Ние се
             стремим да предложим най-добрата стойност за вашите нужди и бюджет."</p>
         </div>
         <div>
           <div
-            class="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
+            class="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 bg-primary-900">
             <i class="fa-regular fa-clock text-2xl md:text-5xl"></i>
           </div>
-          <h3 class="mb-2 text-xl font-bold dark:text-white">На време</h3>
-          <p class="text-gray-500 dark:text-gray-400">Ние предлагаме изключителната способност да завършваме, започнатаите
+          <h3 class="mb-2 text-xl font-bold text-white">На време</h3>
+          <p class="text-gray-400">Ние предлагаме изключителната способност да завършваме, започнатаите
             проекти точно навреме, без забавяния във времето.</p>
         </div>
         <div>
           <div
-            class="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
+            class="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 bg-primary-900">
             <i class="fa-regular fa-eye text-2xl md:text-5xl"></i>
 
           </div>
-          <h3 class="mb-2 text-xl font-bold dark:text-white">Око за детайла</h3>
-          <p class="text-gray-500 dark:text-gray-400">Ние предлагаме изключително око за детайла, което
+          <h3 class="mb-2 text-xl font-bold text-white">Око за детайла</h3>
+          <p class="text-gray-400">Ние предлагаме изключително око за детайла, което
             гарантира, че всеки аспект на проекта е внимателно изпълнен с най-висок стандарт на качество.</p>
         </div>
         <div>
           <div
-            class="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
+            class="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 bg-primary-900">
             <i class="fa-solid fa-house text-2xl md:text-5xl"></i>
           </div>
-          <h3 class="mb-2 text-xl font-bold dark:text-white">Обем на работа</h3>
-          <p class="text-gray-500 dark:text-gray-400">Не се страхуваме да се изправим пред големи и сложни
+          <h3 class="mb-2 text-xl font-bold text-white">Обем на работа</h3>
+          <p class="text-gray-400">Не се страхуваме да се изправим пред големи и сложни
             предизвикателства, защото ние сме ангажирани да доставим изключителни резултати при всяка задача, независимо
             колко голяма и сложна e тя.
           </p>
@@ -146,10 +139,10 @@
     </div>
   </section>
 
-  <section class="bg-white dark:bg-gray-900">
+  <section class="bg-gray-900">
     <div class="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6">
-      <div class="font-light text-gray-500 sm:text-lg dark:text-gray-400">
-        <h2 class="mb-4 text-4xl font-extrabold text-gray-900 dark:text-white">Качество на конкурентни цени</h2>
+      <div class="font-light sm:text-lg text-gray-400">
+        <h2 class="mb-4 text-4xl font-extrabold text-white">Качество на конкурентни цени</h2>
         <p class="mb-4">Ние вярваме в индивидуалността и иновациите, затова с нас ще намерите разнообразие от стилове и
           дизайни, които да отразяват вашите уникални предпочитания и стил.
         </p>
@@ -164,7 +157,7 @@
     </div>
   </section>
 
-  <section class="bg-white dark:bg-gray-900">
+  <section class="bg-gray-900">
     <div class="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
       <div class="mx-auto max-w-screen-sm text-center">
         <h2 class="mb-4 text-2xl font-bold leading-tight text-white">За повече информация очакваме
